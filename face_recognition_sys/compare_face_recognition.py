efdaf91d1f5e90b9839221b9253797e15072b8c4
ruman <rumancha12@gmail.com>
2024-12-24 14:05:14 +0545

    added all files

--- face_recognition_sys/compare_face_recognition.py
+++ face_recognition_sys/compare_face_recognition.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-
 import cv2
 import face_recognition
 import numpy as np
@@ -10,30 +9,34 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 print("Loading Encode File ...")
-file = open('EncodeFile.p', 'rb')
-encode_list_known_with_names = pickle.load(file=file)
-encode_list_known, name_list = encode_list_known_with_names
+with open('EncodeFile.p', 'rb') as file:
+    encode_list_known, name_list = pickle.load(file)
 print("Encode File Loaded")
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    if not success:
+        continue
+
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25, interpolation=cv2.INTER_AREA)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    
+    face_cur_frame = face_recognition.face_locations(imgS, model='hog')
+    if face_cur_frame:
+        encode_cur_frame = face_recognition.face_encodings(imgS, face_cur_frame)
 
-    face_cur_frame = face_recognition.face_locations(imgS)
-    encode_cur_frame = face_recognition.face_encodings(imgS, face_cur_frame)
+        for encode_face, face_loc in zip(encode_cur_frame, face_cur_frame):
+            matches = face_recognition.compare_faces(encode_list_known, encode_face, tolerance=0.6)
+            face_dis = face_recognition.face_distance(encode_list_known, encode_face)
+            
+            if any(matches):
+                match_index = np.argmin(face_dis)
+                if matches[match_index]:
+                    name = name_list[match_index]
+                    print(f"Known face detected: {name}")
 
-    for encode_face, face_loc in zip(encode_cur_frame, face_cur_frame):
-        matches = face_recognition.compare_faces(encode_list_known, encode_face)
-        face_dis = face_recognition.face_distance(encode_list_known, encode_face)
-        print("matches", matches)
-        print("face_distace", face_dis)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        match_index = np.argmin(face_dis)
-
-        if matches[match_index]:
-            print("Known face detected")
-            print(name_list[match_index])
-
-    cv2.imshow("Face Attendance", img)
-    cv2.waitKey(1)
+cap.release()
+cv2.destroyAllWindows()
